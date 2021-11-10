3db5df6b63f33d6b77312662ea6bacde5f977442
LorenzoMinto <lminto@brave.com>
2021-11-10 19:13:27 +0100

    Add DataService and DataStores functionality to BraveFederatedLearningService

--- components/brave_federated/brave_federated_service.h
+++ components/brave_federated/brave_federated_service.h
@@ -3,25 +3,29 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BRAVE_COMPONENTS_BRAVE_FEDERATED_LEARNING_BRAVE_FEDERATED_LEARNING_SERVICE_H_
-#define BRAVE_COMPONENTS_BRAVE_FEDERATED_LEARNING_BRAVE_FEDERATED_LEARNING_SERVICE_H_
+#ifndef BRAVE_COMPONENTS_BRAVE_FEDERATED_BRAVE_FEDERATED_SERVICE_H_
+#define BRAVE_COMPONENTS_BRAVE_FEDERATED_BRAVE_FEDERATED_SERVICE_H_
 
 #include <memory>
 #include <string>
 
+#include "base/files/file_path.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
-
-class PrefService;
-class PrefRegistrySimple;
 
 namespace network {
 class SharedURLLoaderFactory;
 }  // namespace network
 
+class PrefService;
+class PrefRegistrySimple;
+
 namespace brave {
 
-class BraveOperationalPatterns;
+namespace federated {
+
+class DataStoreService;
+class OperationalPatterns;
 
 // In the absence of user data collection, Brave is unable to support learning
 // and decisioning systems for tasks such as private ad matching or private news
@@ -29,26 +33,29 @@ class BraveOperationalPatterns;
 // private federated learning platform, to unlock the value of user generated
 // data in a secure and privacy preserving manner. This component provides the
 // necessary functionality to adopter applications.
-class BraveFederatedLearningService : public KeyedService {
+class BraveFederatedService : public KeyedService {
  public:
-  BraveFederatedLearningService(
+  BraveFederatedService(
       PrefService* prefs,
       PrefService* local_state,
+      const base::FilePath& brave_federated_path,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
-  ~BraveFederatedLearningService() override;
+  ~BraveFederatedService() override;
 
-  BraveFederatedLearningService(const BraveFederatedLearningService&) = delete;
-  BraveFederatedLearningService& operator=(
-      const BraveFederatedLearningService&) = delete;
+  BraveFederatedService(const BraveFederatedService&) = delete;
+  BraveFederatedService& operator=(const BraveFederatedService&) = delete;
 
   static void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
   void Start();
 
+  std::shared_ptr<DataStoreService> GetDataStoreService();
+
  private:
   void InitPrefChangeRegistrar();
   void OnPreferenceChanged(const std::string& key);
 
+  bool IsFederatedLearningEnabled();
   bool ShouldStartOperationalPatterns();
   bool IsP3AEnabled();
   bool IsOperationalPatternsEnabled();
@@ -56,10 +63,15 @@ class BraveFederatedLearningService : public KeyedService {
   PrefService* prefs_;
   PrefService* local_state_;
   PrefChangeRegistrar local_state_change_registrar_;
-  std::unique_ptr<BraveOperationalPatterns> operational_patterns_;
+  const base::FilePath& brave_federated_path_;
+
+  std::unique_ptr<OperationalPatterns> operational_patterns_;
+  std::shared_ptr<DataStoreService> data_store_service_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
 };
 
+}  // namespace federated
+
 }  // namespace brave
 
-#endif  // BRAVE_COMPONENTS_BRAVE_FEDERATED_LEARNING_BRAVE_FEDERATED_LEARNING_SERVICE_H_
+#endif  // BRAVE_COMPONENTS_BRAVE_FEDERATED_BRAVE_FEDERATED_SERVICE_H_
